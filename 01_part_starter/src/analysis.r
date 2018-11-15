518cb7aee270172fd0c49298fdd161ff2d7368c3
oliver bonhamcarter <obonhamcarter@allegheny.edu>
2018-11-14 22:14:47 -0500

    Update

--- 01_part_starter/src/analysis.r
+++ 01_part_starter/src/analysis.r
@@ -1,16 +1,18 @@
 # Date: 15 Nov 2018
-# Class participation
+# Class participation 1
 
 # These are libraries that you may need.
 library(gapminder)
 library(tidyverse)
 library(psych)
 
+# Add your code to be able to run the script and recreate your work. Your code should run from beginning to end and recreate your all variables including loading the data. Be sure restart R and then re-run your code to determine that all runs smoothly.
+
 
 ########################
 # Load the data
 ########################
-
+# You may need the below line.
 data <- read.table(file.choose(), header = TRUE, sep = ",")
 
 
@@ -22,7 +24,18 @@ data <- read.table(file.choose(), header = TRUE, sep = ",")
 # Analysis by pairs.panels()
 
 
+
+
 ########################
 # Exploratory Plots
 ########################
 # Pick data with a high Correlation from above and show their plot.
+
+
+
+
+########################
+# Linear Model(s)
+########################
+# Use the lm() function.
+# Which are the dependent and independent variables?
---
+++ 01_part_starter/writing/minireport.md
@@ -0,0 +1,7 @@
+# Date: 15 Nov 2018
+# Class participation 1
+
+### Q1: What were you studying?
+
+
+### Q2: What conclusions can you draw from your analysis? Explain.
